8ef2f84a7a5f53aec5f86f41766b2067ba982b66
fgonzalezdiaz <fgonzalezd24042005@gmail.com>
2025-12-30 18:18:25 +0100

    30-12/2025.2 - Creacion de apuntes de PHP + Estructura de carpetas + Conexion con la Database + Comentarios utiles en config/db.php

--- config/db.php
+++ config/db.php
@@ -1,4 +1,10 @@
 <?php
+/*
+    Hemos usado XAMPP para el entorno.
+    Puerto de la database : 3306
+    Puerto del apache : 8888
+    Y la database la hemos creado desde el localhost:8888/phpmyadmin
+*/
  $servername = "localhost";
  $username = "root";
  $password = "";
